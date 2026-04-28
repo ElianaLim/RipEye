ddf3271c5b907f1bf6564f323b47f0b2a9016f32
hugvan <vaughnaquino@gmail.com>
2026-04-28 16:01:22 +0800

    trained new model

--- data/download.py
+++ data/download.py
@@ -1,5 +1,5 @@
 from roboflow import Roboflow
 rf = Roboflow(api_key="WBhh6jyHHZZ6iU1N2atG")
-project = rf.workspace("damaged-package").project("damaged-package")
-version = project.version(2)
-dataset = version.download("tfrecord")
+project = rf.workspace("aadhavs-first-workspace").project("damaged-vs-good-packages")
+version = project.version(1)
+dataset = version.download("folder")
