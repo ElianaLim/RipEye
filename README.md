2e3150f7dc770682545cbb7afc850908f650a606
Charlize Sim <cssim1@up.edu.ph>
2026-04-28 13:20:02 +0800

    add evaluation metrics

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # RipEye
 
-
-
 Install the required libraries using the provided requirements file:
 ```bash
 pip install -r requirements.txt
 ```
+
+Run `download.py` in the `data` directory to download the dataset from Roboflow 
--- data/download.py
+++
@@ -1,5 +0,0 @@
-from roboflow import Roboflow
-rf = Roboflow(api_key="WBhh6jyHHZZ6iU1N2atG")
-project = rf.workspace("damaged-package").project("damaged-package")
-version = project.version(2)
-dataset = version.download("tfrecord")
